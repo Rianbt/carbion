acd5bae7650b7083ff5e0a55d1979db763f4723a
Rianbt <rianzinho1204@gmail.com>
2025-09-25 12:40:41 -0300

    atual

---
+++ models/User.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+const User = mongoose.model('User', {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+});
+
+export default User;
---
+++ server.js
@@ -0,0 +1,146 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const port = 3000;
+
+//JSON config
+app.use(express.json());
+
+//Models 
+
+import User from './models/User.js';
+
+//open route - public route
+app.get('/', (req, res) => {
+  res.status(200).json({msg: 'bem vindo ao carbion'});
+});
+
+// private route - precisa de token
+app.get('/user/:id', async (req, res) => {
+    const id = req.params.id;
+
+    // checar se o usuario existe
+    const user = await User.findById(id);
+    if(!user) {
+        return res.status(404).json({msg: 'Usuário não encontrado'});
+    }
+    res.status(200).json({user});
+});
+
+function checkToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if(!token) {
+        return res.status(401).json({msg: 'Acesso negado'});
+    } 
+
+    try {
+        const secret = process.env.SECRET;
+
+        const decoded = jwt.verify(token, secret);
+        
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(403).json({msg: 'Token inválido'});
+    }
+}
+
+//login route
+app.post('/auth/login', async (req, res) => {
+
+    const {email, password, name} = req.body;
+
+    //validações
+    if(!email || !password || !name) {
+        return res.status(400).json({msg: 'Todos os campos são obrigatórios'});
+    }
+
+    // checar se usuario existe
+    const user = await User 
+        .findOne({email: email})
+        .select('-__v');
+
+    if(!user) {
+        return res.status(404).json({msg: 'Usuário não encontrado'});
+    }
+
+    // checar se a senha esta correta
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch) {
+        return res.status(422).json({msg: 'Senha inválida'});
+    }
+
+    // criar token
+
+    try {
+        const secret = process.env.SECRET;
+        const token = jwt.sign(
+            {
+                id: user._id,
+            },
+            secret,
+        );
+        res.status(200).json({msg: 'Login realizado com sucesso', token});
+    } catch (error) {
+        res.status(500).json({msg: 'Erro ao gerar token'});
+    }
+});
+
+
+// registrar usuario
+app.post('/auth/register', async (req, res) => {
+
+    const {name, email, password, confirmpassword} = req.body;  
+
+    //validações
+    if(!name || !email || !password || !confirmpassword) {
+        return res.status(400).json({msg: 'Todos os campos são obrigatórios'});
+    }
+
+    if(password !== confirmpassword) {
+        return res.status(400).json({msg: 'As senhas não conferem'});
+    }
+
+    // check if user exists
+    const userExists = await User.findOne({email: email});
+    if(userExists) {
+        return res.status(400).json({msg: 'Por favor, utilize outro e-mail'});
+    };
+
+    // create password
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+
+    // create user
+    const user = new User({
+        name,
+        email,
+        password: passwordHash
+    });
+
+    try {
+        await user.save();
+        res.status(201).json({msg: 'Usuário registrado com sucesso'});
+    } catch (error) {
+        res.status(500).json({msg: 'Erro ao registrar usuário'});
+    }
+});
+
+//credenciais
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.j6zsxa5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
+}).catch((err) => console.log(err));
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
